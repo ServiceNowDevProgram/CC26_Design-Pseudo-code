87262508598241d2f93f9b9dc11a6efa5f575849
Laszlo <47461634+Lacah@users.noreply.github.com>
2026-03-31 14:56:19 +0200

    Add initial hackathon.js implementation

---
+++ hackathon.js
@@ -0,0 +1,36 @@
+// --- #1 ---
+let plan = hackathon.scope();
+let reality = plan * 0.3;
+let demo = reality + confidence++;
+
+// --- #2 ---
+if (idea.isStupid() && idea.worksAnyway()) {
+    return "ship it";
+}
+
+// --- #3 ---
+const hackathonTeam = {
+    designer: "also codes",
+    developer: "also designs",
+    presenter: "was asleep 5 min ago",
+    wildcard: "joined 20 min ago, already shipping"
+};
+
+// --- #4 ---
+function hackathon(team) {
+    let sleep = null;
+    let ego = false;
+    let snacks = Infinity;
+
+    return team.build(something.amazing);
+}
+
+// --- #5 ---
+while (coffee.level > 0) {
+    hackathon.ship();
+}
+// TODO: handle empty coffee edge case
+// UPDATE: there is no edge case. we simply stop.
+
+// --- #6 ---
+
